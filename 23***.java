a16d2cfbd380b28fcf3e93e385d61886d418c36f
Wang Mingwei <cloveriiw@gmail.com>
2017-04-08 15:28:44 +0800

    170408-1-3

---
+++ 23***.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode(-1), cur = head;
+        if(lists.length == 0) return null;
+        PriorityQueue<ListNode> q = new PriorityQueue(lists.length, new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                return Integer.compare(o1.val, o2.val);
+            }
+        });
+        
+        for(ListNode node: lists) {
+            if(node != null) q.add(node); // [[]]
+        }
+        
+        while(!q.isEmpty()) {
+            ListNode node = q.poll();
+            //if(node == null) continue; // unnecessary
+            if(node.next != null) q.add(node.next);
+            cur.next = node;
+            cur = node;
+            //cur.next = null; // unnecessary
+            
+        }
+        return head.next;
+    }
+}
